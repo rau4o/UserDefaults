a1713511be98c6afc5714b84822057a7c1afefae
Rauan <rkoshkinbayev@gmail.com>
2020-04-10 19:31:16 +0600

    Initial Commit

---
+++ SaveDiplom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveDiplom
+//
+//  Created by rau4o on 4/10/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
